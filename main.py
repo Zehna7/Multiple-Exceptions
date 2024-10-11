ab68ef26fd1eb54b67562639a8dd287ce4039f04
Zehna7 <zehna.omar2012@gmail.com>
2024-10-11 17:31:59 +0600

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+try:
+    num1 = int(input("Enter a number: "))
+    num2 = int(input("Enter a number: "))
+    result = num1/num2
+    print("Result is: ", result)
+    print("Result is: ", result1)
+except ZeroDivisionError:
+    print("Division error is not allowed")
+except ValueError:
+    print("Please enter numerical value")
+except NameError as ex:
+    print("The exception is: ", ex)
+except:
+    print("Some error occured")
+finally:
+    print("I will execute no matter what happens")
